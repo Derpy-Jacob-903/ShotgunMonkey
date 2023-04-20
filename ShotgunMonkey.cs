66264f0cc743c24a1f3f33271898fb535d58c4a1
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2023-04-20 08:55:53 -0600

    Update ShotgunMonkey.cs

--- ShotgunMonkey.cs
+++ ShotgunMonkey.cs
@@ -33,7 +33,7 @@ public class ShotgunMonkeyMod : BloonsTD6Mod
         public override int Cost => 450; //400+250-
         public override string Description => "Shotgun Monkey.";
         public override string DisplayName => "Shotgun Monkey";
-        public override int TopPathUpgrades => 0;
+        public override int TopPathUpgrades => 1;
         public override int MiddlePathUpgrades => 0;
         public override int BottomPathUpgrades => 0;
         //public override ParagonMode ParagonMode => ParagonMode.Base555;
@@ -46,11 +46,30 @@ public class ShotgunMonkeyMod : BloonsTD6Mod
 
             var attackModel = towerModel.GetAttackModel();
             attackModel.range = 15;
+            
             attackModel.weapons[0] = Game.instance.model.GetTowerFromId("Druid-010").GetAttackModel().weapons[0].Duplicate();
             //attackModel.AddWeapon(Game.instance.model.GetTowerFromId("Sauda").GetAttackModel().weapons[0].Duplicate());
+            towerModel.GetWeapon().emission = new RandomEmissionModel("RandomEmissionModel_", 8, 0, 45, null, false);
+            ///
+            "emission": {
+            "angle": 35.0,
+            "count": 8,
+            "startOffset": 0.0,
+            "useSpeedMultiplier": false,
+            "speedMultiplierMin": 0.0,
+            "speedMultiplierMax": 0.0,
+            "ejectPointRandomness": 0.0,
+            "useMainAttackRotation": false,
+            "behaviors": null,
+            "name": "RandomEmissionModel_",
+            "ImplementationType": "Assets.Scripts.Simulation.Towers.Emissions.RandomEmission, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null"
+          },
+            ///
             var projectileModel = towerModel.GetAttackModel().GetDescendant<ProjectileModel>();
 
             var projectile = attackModel.weapons[0].projectile;
+            // TODO: Find the shrapnel projectile
+            //attackModel.weapons[0].projectile = Game.instance.model.GetTowerFromId("???").GetAttackModel().weapons[0].projectile.Duplicate();
             projectile.ApplyDisplay<ShrapnelDisplay>();
 
             //projectileModel1.weapons[1].GetDamageModel().immuneBloonProperties = BloonProperties.Black;
@@ -75,6 +94,42 @@ public class ShotgunMonkeyMod : BloonsTD6Mod
             {
 
             }
+        }
+    }
+namespace ShotgunMonkey.Upgrades
+{
+    public class FMJ : ModUpgrade<ShotgunMonkeyMod.ShotgunMonkey>
+    { 
+        public override int Path => TOP;
+        public override int Tier => 1;
+        public override int Cost => 360; //140+220
+        //public override string Portrait => "BlitzaPortrait";
+        public override SpriteReference IconReference => Game.instance.model.GetTowerFromId("MonkeyAce").GetUpgrade(BOTTOM, 1).icon;
+        public override string Description => "Pellets can pop Lead Bloons";
+        //public override string DisplayName => "FMJ";
+
+        public override void ApplyUpgrade(TowerModel towerModel)
+        {
+            var attackModel = towerModel.GetAttackModel();
+            var projectile = attackModel.weapons[0].projectile;
+            towerModel.GetAttackModel().weapons[0].projectile.GetDamageModel().immuneBloonProperties = BloonProperties.Lead;
+        }
+    }
+    public class FMJ : ModUpgrade<ShotgunMonkeyMod.ShotgunMonkey>
+    { 
+        public override int Path => TOP;
+        public override int Tier => 1;
+        public override int Cost => 360; //140+220
+        //public override string Portrait => "BlitzaPortrait";
+        public override SpriteReference IconReference => Game.instance.model.GetTowerFromId("MonkeyAce").GetUpgrade(BOTTOM, 1).icon;
+        public override string Description => "Pellets can pop Lead Bloons";
+        //public override string DisplayName => "FMJ";
+
+        public override void ApplyUpgrade(TowerModel towerModel)
+        {
+            var attackModel = towerModel.GetAttackModel();
+            var projectile = attackModel.weapons[0].projectile;
+            towerModel.GetAttackModel().weapons[0].projectile.GetDamageModel().immuneBloonProperties = BloonProperties.Lead;
         }
     }
 }
