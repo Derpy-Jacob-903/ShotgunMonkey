6051b2d89f227fb2177e9e66dc51141db8a2b2e9
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2023-04-19 22:43:58 -0500

    Shotgun Monkey 1.1

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,11 +3,11 @@ namespace ShotgunMonkey;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "36.1";
-    public const string Version = "0.0.1";
+    public const string Version = "InDev"; //0.0.1
     public const string Name = "Shotgun Monkey";
 
     public const string Description = "Adds a monkey with a shotgun.";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "Derpy-Jacob-903"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "ShotgunMonkey"; // TODO add your repo name here, also in the download url in README.md
 }
