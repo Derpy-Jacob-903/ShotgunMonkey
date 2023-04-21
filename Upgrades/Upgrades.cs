72852e8a198be89011d41747c33a9f4f9cc18bf9
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2023-04-21 13:58:56 -0600

    Update Upgrades.cs

--- Upgrades/Upgrades.cs
+++ Upgrades/Upgrades.cs
@@ -206,12 +206,13 @@ namespace ShotgunMonkey.Upgrades
             var rateDown = towerModel.GetWeapon().rate
             rateDown *= -0.6f
             towerModel.GetWeapon().rate *= 0.4f;
-            towerModel.AddBehavior(new DamageBasedAttackSpeedModel();
-            "damageThreshold": 1.0,
-            "increasePerThreshold": rateDown,
-            "maxStacks": 1,
-            "maxTimeInFramesWithoutDamage": 199,
-            "name": "DamageBasedAttackSpeedModel_",
+            //towerModel.AddBehavior(new DamageBasedAttackSpeedModel();
+            //
+            //"damageThreshold": 1.0,
+            //"increasePerThreshold": rateDown,
+            //"maxStacks": 1,
+            //"maxTimeInFramesWithoutDamage": 199,
+            //"name": "DamageBasedAttackSpeedModel_",
             //towerModel.GetWeapon().rate *= 0.5f;
             //.7
 
