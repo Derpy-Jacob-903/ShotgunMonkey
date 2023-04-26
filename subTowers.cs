82e26b71bb9eadd0b69375534559623b642fe8d8
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2023-04-26 11:57:01 -0600

    Update subTowers.cs

--- subTowers.cs
+++ subTowers.cs
@@ -30,7 +30,7 @@ namespace ShotgunMonkey.subTowers;
     {
         //public override string Name => "Shotgun Monkey";
         public override TowerSet TowerSet => TowerSet.Support;
-        public override string BaseTower => TowerType.DartMonkey;
+        public override string BaseTower => TowerType.Sentry;
         public override int Cost => 450; //400+250-
         public override string Description => "Shotgun Monkey.";
         public override string DisplayName => "Shotgun Sentry";
@@ -66,15 +66,15 @@ namespace ShotgunMonkey.subTowers;
     {
         //public override string Name => "Shotgun Monkey";
         public override TowerSet TowerSet => TowerSet.Support;
-        public override string BaseTower => TowerType.DartMonkey;
+        public override string BaseTower => TowerType.SentryCold;
         public override int Cost => 450; //400+250-
         public override string Description => "Shotgun Monkey.";
         public override string DisplayName => "Buckshot Sentry";
         public override int TopPathUpgrades => 0;
         public override int MiddlePathUpgrades => 0;
         public override int BottomPathUpgrades => 0;
-        public override string Icon => VanillaSprites.SentryPortrait;
-        public override string Portrait => VanillaSprites.SentryPortrait;
+        public override string Icon => VanillaSprites.SentryColdPortrait;
+        public override string Portrait => VanillaSprites.SentryColdPortrait;
         //public override ParagonMode ParagonMode => ParagonMode.Base555;
         public override void ModifyBaseTowerModel(TowerModel towerModel)
         {
@@ -105,15 +105,15 @@ namespace ShotgunMonkey.subTowers;
     {
         //public override string Name => "Shotgun Monkey";
         public override TowerSet TowerSet => TowerSet.Support;
-        public override string BaseTower => TowerType.DartMonkey;
+        public override string BaseTower => TowerType.SentryCrushing;
         public override int Cost => 450; //400+250-
         public override string Description => "Fires long-range Slugs";
         public override string DisplayName => "Slug Sentry";
         public override int TopPathUpgrades => 0;
         public override int MiddlePathUpgrades => 0;
         public override int BottomPathUpgrades => 0;
-        public override string Icon => VanillaSprites.SentryPortrait;
-        public override string Portrait => VanillaSprites.SentryPortrait;
+        public override string Icon => VanillaSprites.SentryCrushingPortrait;
+        public override string Portrait => VanillaSprites.SentryCrushingPortrait;
         //public override ParagonMode ParagonMode => ParagonMode.Base555;
         public override void ModifyBaseTowerModel(TowerModel towerModel)
         {
@@ -141,15 +141,15 @@ namespace ShotgunMonkey.subTowers;
     {
         //public override string Name => "Shotgun Monkey";
         public override TowerSet TowerSet => TowerSet.Support;
-        public override string BaseTower => TowerType.DartMonkey;
+        public override string BaseTower => TowerType.SentryBoom;
         public override int Cost => 450; //400+250-
         public override string Description => "Fires long-range Slugs";
-        public override string DisplayName => "Ex. Slug Sentry";
+        public override string DisplayName => "Boom Slug Sentry";
         public override int TopPathUpgrades => 0;
         public override int MiddlePathUpgrades => 0;
         public override int BottomPathUpgrades => 0;
-        public override string Icon => VanillaSprites.SentryPortrait;
-        public override string Portrait => VanillaSprites.SentryPortrait;
+        public override string Icon => VanillaSprites.SentryBoomPortrait;
+        public override string Portrait => VanillaSprites.SentryBoomPortrait;
         //public override ParagonMode ParagonMode => ParagonMode.Base555;
         public override void ModifyBaseTowerModel(TowerModel towerModel)
         {
@@ -193,15 +193,15 @@ namespace ShotgunMonkey.subTowers;
     {
         //public override string Name => "Shotgun Monkey";
         public override TowerSet TowerSet => TowerSet.Support;
-        public override string BaseTower => TowerType.DartMonkey;
+        public override string BaseTower => TowerType.SentryEnergy;
         public override int Cost => 450; //400+250-
         public override string Description => "Fires incendiary ammunition, burning Bloons over time.\nCan't pop Purple Bloons";
         public override string DisplayName => "Incendiary Shotgun Sentry";
         public override int TopPathUpgrades => 0;
         public override int MiddlePathUpgrades => 0;
         public override int BottomPathUpgrades => 0;
-        public override string Icon => VanillaSprites.SentryPortrait;
-        public override string Portrait => VanillaSprites.SentryPortrait;
+        public override string Icon => VanillaSprites.SentryEnergyPortrait;
+        public override string Portrait => VanillaSprites.SentryEnergyPortrait;
         //public override ParagonMode ParagonMode => ParagonMode.Base555;
         public override void ModifyBaseTowerModel(TowerModel towerModel)
         {
@@ -213,21 +213,7 @@ namespace ShotgunMonkey.subTowers;
 
             var attackModel = towerModel.GetAttackModel();
             attackModel.range = 20;
-
-            attackModel.weapons[0].projectile = Game.instance.model.GetTowerFromId("SniperMonkey-020").GetAttackModel().GetDescendant<ProjectileModel>().GetDescendant<EmitOnDamageModel>().GetDescendant<ProjectileModel>().Duplicate(); //Gets the
-            //projectile.pierce = 8;
-            towerModel.GetWeapon().emission = new RandomEmissionModel("RandomEmissionModel_", 8, 60f, 0f, null, false, 1f, 1f, 1f, false);
-            towerModel.GetWeapon().rate = Game.instance.model.GetTowerFromId("SniperMonkey").GetAttackModel().weapons[0].rate;
-
-
-            var projectile = attackModel.weapons[0].projectile;
-            var projectileModel = towerModel.GetAttackModel().GetDescendant<ProjectileModel>();
-
-            var fireProjectile = Game.instance.model.GetTowerFromId("Gwendolin 6").GetAttackModel().weapons[0].projectile.Duplicate();
-            projectile.AddBehavior(fireProjectile.GetBehavior<AddBehaviorToBloonModel>());
-            projectile.GetDamageModel().immuneBloonProperties = BloonProperties.Purple;
-            projectile.ApplyDisplay<FireDisplay>();
-            
+            GetTowerModel<ShotgunMonkey>().GetAttackModel();
         }
     }
 }
