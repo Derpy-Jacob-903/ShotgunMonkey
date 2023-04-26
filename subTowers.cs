80a965fb1cda5d5dcd7b28f6a460f6fa99235de7
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2023-04-26 10:56:55 -0600

    Update subTowers.cs

--- subTowers.cs
+++ subTowers.cs
@@ -230,46 +230,4 @@ namespace ShotgunMonkey.subTowers;
             
         }
     }
-    public class ThermalSentry : ModTower
-    {
-        //public override string Name => "Shotgun Monkey";
-        public override TowerSet TowerSet => TowerSet.Support;
-        public override string BaseTower => TowerType.DartMonkey;
-        public override int Cost => 450; //400+250-
-        public override string Description => "Fires long-range Slugs";
-        public override string DisplayName => "Thermal Sentry";
-        public override int TopPathUpgrades => 0;
-        public override int MiddlePathUpgrades => 0;
-        public override int BottomPathUpgrades => 0;
-        public override string Icon => VanillaSprites.SentryPortrait;
-        public override string Portrait => VanillaSprites.SentryPortrait;
-        //public override ParagonMode ParagonMode => ParagonMode.Base555;
-        public override void ModifyBaseTowerModel(TowerModel towerModel)
-        {
-
-            //towerModel.GetBehavior<DisplayModel>().display = towerModel.display;
-            //towerModel.ApplyDisplay<TowerDisplays.Display000>();
-            //Game.instance.model.GetTower("Sentry").display.GUID
-            towerModel.range = 20;
-
-            var attackModel = towerModel.GetAttackModel();
-            attackModel.range = 20;
-
-            attackModel.weapons[0].projectile = Game.instance.model.GetTowerFromId("SniperMonkey-020").GetAttackModel().GetDescendant<ProjectileModel>().GetDescendant<EmitOnDamageModel>().GetDescendant<ProjectileModel>().Duplicate(); //Gets the
-
-            var projectileModel = towerModel.GetAttackModel().GetDescendant<ProjectileModel>();
-            var projectile = attackModel.weapons[0].projectile;
-            projectile.pierce = 1;
-            projectile.maxPierce = 1;
-            //towerModel.GetWeapon().emission = new RandomEmissionModel("RandomEmissionModel_", 8, 60f, 0f, null, false, 1f, 1f, 1f, false);
-            towerModel.GetWeapon().rate = Game.instance.model.GetTowerFromId("SniperMonkey").GetAttackModel().weapons[0].rate;
-            projectile.GetBehavior<TravelStraitModel>().Lifespan *= 3;
-            var bombProjectile = Game.instance.model.GetTowerFromId("BombShooter-200").GetAttackModel().weapons[0].projectile.Duplicate();
-            var fireProjectile = Game.instance.model.GetTowerFromId("WizardMonkey-020").GetAttackModel().weapons[0].projectile.Duplicate();
-            projectile.AddBehavior(bombProjectile.GetBehavior<CreateProjectileOnContactModel>());            
-            projectile.GetBehavior<CreateProjectileOnContactModel>().projectile = fireProjectile;
-            //projectile.GetBehavior<CreateProjectileOnContactModel>().projectile
-            projectile.ApplyDisplay<BombDisplay>();
-            
-        }
-    }
+}
